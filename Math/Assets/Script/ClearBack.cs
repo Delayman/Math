712e6d0e9dc15867e8ee79dea94ddb2d1602d40c
LostzSoul <61343588+LostzSoul@users.noreply.github.com>
2020-11-25 22:46:31 +0700

    Complete

---
+++ Math/Assets/Script/ClearBack.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClearBack : MonoBehaviour
+{
+    public void Clear()
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+    public void Back()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+}
---
+++ Math/Assets/Script/StartQuit.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartQuit : MonoBehaviour
+{
+
+    public void Begin()
+    {
+        SceneManager.LoadScene("Main");
+
+    }
+    public void Quit()
+    {
+        Debug.Log("Quit");
+
+        Application.Quit();
+
+    }
+}
